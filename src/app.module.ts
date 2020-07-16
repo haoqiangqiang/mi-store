61de7a259a79154b75e4e1fad2f0be9a6eed6b2a
haoqiangqiang <826173335@qq.com>
2020-07-16 23:21:34 +0800

    搭建架构

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AdminModule } from './module/admin/admin.module';
+import { DefaultModule } from './module/default/default.module';
+import { ApiModule } from './module/api/api.module';
+import { ToolsService } from './service/tools/tools.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AdminModule, DefaultModule, ApiModule],
+  controllers: [],
+  providers: [ToolsService],
 })
 export class AppModule {}
